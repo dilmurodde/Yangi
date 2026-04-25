01f10f45d7567ad5327caac88c3e881ca3d32f8f
dilmurodde <dilidilhush@gmail.com>
2026-04-25 22:49:49 +0500

    Add configuration for API token, languages, and regions

---
+++ config.py
@@ -0,0 +1,16 @@
+API_TOKEN = "8253888597:AAHgdufA4zg1DKmnt_C3mQ19RP9updrLRAQ"
+ADMIN_ID = 123456789  # O'zingizning ID raqamingiz
+
+DB_NAME = "dating_bot.db"
+
+LANGUAGES = {
+    "uz": "O'zbekcha 🇺🇿",
+    "en": "English 🇺🇸",
+    "ru": "Русский 🇷🇺"
+}
+
+REGIONS = [
+    "Toshkent", "Andijon", "Buxoro", "Farg'ona", "Jizzax", 
+    "Xorazm", "Namangan", "Navoiy", "Qashqadaryo", 
+    "Samarqand", "Sirdaryo", "Surxondaryo", "Qoraqalpog'iston"
+]
